eca1aa0ccaa53164be9d5bf7e0848185da210f54
Matias Reyes <matiasreyes3172@gmail.com>
2024-03-31 20:28:02 -0300

    Se agrega Menu + opciones

--- Proyecto1Buses/src/main/java/com/mycompany/proyecto1buses/Proyecto1Buses.java
+++ Proyecto1Buses/src/main/java/com/mycompany/proyecto1buses/Proyecto1Buses.java
@@ -12,49 +12,76 @@ public class Proyecto1Buses {
         cargarDestinos();
 
         Scanner scanner = new Scanner(System.in);
+        int opcion;
+        
+        do {
 
-        //Aqui mostramos las opciones de destino
-        System.out.println("Seleccione un destino:");
-        for (int i = 0; i < destinos.size(); i++) {
-            System.out.println((i + 1) + ". " + destinos.get(i).getNombre());
-        }
+        System.out.println("\nMenu:");
+            System.out.println("1. Comprar boleto");
+            System.out.println("2. Salir");
+            System.out.print("Ingrese su opcion: ");
 
-        int opcionDestino = scanner.nextInt();
-        Destino destinoElegido = destinos.get(opcionDestino - 1);
+            opcion = scanner.nextInt();
+            switch (opcion) {
+                
+                case 1:
+                    //Aqui mostramos las opciones de destino
+                    System.out.println("Seleccione un destino:");
+                    for (int i = 0; i < destinos.size(); i++) {
+                        System.out.println((i + 1) + ". " + destinos.get(i).getNombre());
+                    }
 
-        //Aqui las opciones de horario
-        System.out.println("Seleccione un horario para " + destinoElegido.getNombre() + ":");
-        for (Horario horario : destinoElegido.getHorarios()) {
-            System.out.println(horario.getHora() + " - " + (horario.getCupoDisponible() > 0 ? "Disponible (" + horario.getCupoDisponible() + " cupos)" : "Completo"));
-        }
+                    int opcionDestino = scanner.nextInt();
+                    Destino destinoElegido = destinos.get(opcionDestino - 1);
 
-        int opcionHorario = scanner.nextInt();
-        Horario horarioElegido = destinoElegido.getHorarios().get(opcionHorario - 1);
+                    //Aqui las opciones de horario
+                    System.out.println("Seleccione un horario para " + destinoElegido.getNombre() + ":");
+                    for (Horario horario : destinoElegido.getHorarios()) {
+                        System.out.println(horario.getHora() + " - " + (horario.getCupoDisponible() > 0 ? "Disponible (" + horario.getCupoDisponible() + " cupos)" : "Completo"));
+                    }
 
-        // Validamos los cupos disponibles
-        if (horarioElegido.getCupoDisponible() <= 0) {
-            System.out.println("Lo sentimos, no hay cupos disponibles para ese horario.");
-            return;
-        }
+                    int opcionHorario = scanner.nextInt();
+                    Horario horarioElegido = destinoElegido.getHorarios().get(opcionHorario - 1);
 
-        // Se registra la compra
-        System.out.println("Ingrese su nombre:");
-        String nombre = scanner.next();
+                    // Validamos los cupos disponibles
+                    if (horarioElegido.getCupoDisponible() <= 0) {
+                        System.out.println("Lo sentimos, no hay cupos disponibles para ese horario.");
+                        return;
+                    }
 
-        System.out.println("Ingrese su apellido:");
-        String apellido = scanner.next();
+                    // Se registra la compra
+                    System.out.println("Ingrese su nombre:");
+                    String nombre = scanner.next();
 
-        System.out.println("Ingrese su RUT:");
-        String rut = scanner.next();
+                    System.out.println("Ingrese su apellido:");
+                    String apellido = scanner.next();
 
-        Compra compra = new Compra(nombre, apellido, rut, destinoElegido, horarioElegido);
-        System.out.println("Compra registrada exitosamente para " + compra.getDestino().getNombre() + " a las " + compra.getHorario().getHora());
+                    System.out.println("Ingrese su RUT:");
+                    String rut = scanner.next();
 
-        //Descontamos el cupo
-        horarioElegido.setCupoDisponible(horarioElegido.getCupoDisponible() - 1);
+                    Compra compra = new Compra(nombre, apellido, rut, destinoElegido, horarioElegido);
+                    System.out.println("Compra registrada exitosamente para " + compra.getDestino().getNombre() + " a las " + compra.getHorario().getHora());
 
-        System.out.println("Cupos disponibles para " + destinoElegido.getNombre() + " a las " + horarioElegido.getHora() + ": " + horarioElegido.getCupoDisponible());
+                    //Descontamos el cupo
+                    horarioElegido.setCupoDisponible(horarioElegido.getCupoDisponible() - 1);
+
+                    System.out.println("Cupos disponibles para " + destinoElegido.getNombre() + " a las " + horarioElegido.getHora() + ": " + horarioElegido.getCupoDisponible());
+                    
+                    break;  
+                case 2:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opcion invalida. Intente nuevamente.");
+            }
+        } while(opcion != 2);
+        scanner.close();
     }
+    
+    
+    
+    
+    
 
     private static void cargarDestinos() {
         // Aqui agregamos los destinos y sus terminales anidados aquí
